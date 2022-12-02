267a012be47bf815e24275e10c5b8aa1b5b001db
Sam Bencivengo <sambencivengo@gmail.com>
2022-12-02 10:52:57 -0700

    initial server for docker testing

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (_, res) => {
+	res.send('Hello');
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port: ${PORT}`);
+});
